a149f0142be844e13c77b937ced8c616de24920b
2022101362 <2022101362@LAB2-018>
2023-08-22 20:04:44 -0300

    Adicionar arquivos de projeto.

---
+++ ConceitosBasicos/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConceitosBasicos;
+
+int num1 = 20;
+int num2 = 11, result;
+
+Somador soma;
+soma = new Somador();
+
+result = soma.Soma(num1, num2);
+
+Conversor conversor = new Conversor();
+
+Console.WriteLine($"A soma dos números{num1}+{num2} é {result}");
+
+double metros= 1.76;
+Console.WriteLine("A medida {0} metros corresponde a {1}",metros,Conversor.MetrosMilimetros(1.76));
+
